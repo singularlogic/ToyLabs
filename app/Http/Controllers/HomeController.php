db609f350bb1d1f654a47edbc922249c0b9ade3c
Marios Phinikettos <finikm@gmail.com>
2017-07-07 11:42:49 +0300

    Prototypes and Designs are shuffled into homepage as well

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,76 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Design;
 use App\Product;
+use App\Prototype;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $products = Product::where('is_public', true)->get();
+        $products   = Product::where('is_public', true)->get();
+        $designs    = Design::where('is_public', true)->get();
+        $prototypes = Prototype::where('is_public', true)->get();
+        $products   = $products->merge($designs);
+        $products   = $products->merge($prototypes);
+        $products   = $products->shuffle();
 
         $data = [
             'products' => $products,
         ];
-
-        // $data = [
-        //     "products" => [
-        //         [
-        //             "id"          => 1,
-        //             "type"        => "product",
-        //             "title"       => "Product Title",
-        //             "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        //             "comments"    => 3,
-        //             "likes"       => 8,
-        //             "image"       => "/images/placeholder.jpg",
-        //         ],
-        //         [
-        //             "id"          => 2,
-        //             "type"        => "prototype",
-        //             "title"       => "Prototype Title",
-        //             "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        //             "comments"    => 8,
-        //             "likes"       => 2,
-        //             "image"       => "/images/placeholder.jpg",
-        //         ],
-        //         [
-        //             "id"          => 3,
-        //             "type"        => "design",
-        //             "title"       => "Design Title",
-        //             "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        //             "comments"    => 13,
-        //             "likes"       => 0,
-        //             "image"       => "/images/placeholder.jpg",
-        //         ],
-        //         [
-        //             "id"          => 4,
-        //             "type"        => "design",
-        //             "title"       => "Design Title 2",
-        //             "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        //             "comments"    => 2,
-        //             "likes"       => 0,
-        //             "image"       => "/images/placeholder.jpg",
-        //         ],
-        //         [
-        //             "id"          => 5,
-        //             "type"        => "design",
-        //             "title"       => "Design Title 3",
-        //             "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        //             "comments"    => 1,
-        //             "likes"       => 1,
-        //             "image"       => "/images/placeholder.jpg",
-        //         ],
-        //         [
-        //             "id"          => 6,
-        //             "type"        => "prototype",
-        //             "title"       => "Prototype Title 4",
-        //             "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        //             "comments"    => 0,
-        //             "likes"       => 0,
-        //             "image"       => "/images/placeholder.jpg",
-        //         ],
-        //     ],
-        // ];
 
         return view('home', $data);
     }
