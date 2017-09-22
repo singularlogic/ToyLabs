c9677d33984f74a3cd084c5c18028dbb0b3e7bc9
Marios Phinikettos <finikm@gmail.com>
2017-09-22 18:37:43 +0300

    Refactoring Partner Matching

--- resources/assets/js/components/organizations/Certifications.vue
+++ resources/assets/js/components/organizations/Certifications.vue
@@ -6,7 +6,7 @@
 
 <script>
     export default {
-        props: ['certifications'],
+        props: ['certifications', 'awards'],
         data() {
             return {};
         },
--- resources/assets/js/components/organizations/index.js
+++ resources/assets/js/components/organizations/index.js
@@ -1,6 +1,4 @@
-export const AwardsTab = require('./Awards.vue');
 export const CertificationsTab = require('./Certifications.vue');
 export const FacilitiesTab = require('./Facilities.vue');
 export const GeneralTab = require('./General.vue');
 export const ServicesTab = require('./Services.vue');
-export const TechnicalTab = require('./Technical.vue');
