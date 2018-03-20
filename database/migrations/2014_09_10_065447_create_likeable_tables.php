dead229ea51a269c84048c1ff806103c9dd2dde4
Marios Phinikettos <finikm@gmail.com>
2018-03-20 12:34:04 +0200

    Migration fixes

--- database/migrations/2014_09_10_065447_create_likeable_tables.php
+++ database/migrations/2014_09_10_065447_create_likeable_tables.php
@@ -12,7 +12,7 @@ class CreateLikeableTables extends Migration
         Schema::create('likeable_likes', function (Blueprint $table) {
             $table->increments('id');
             $table->string('likable_id', 36);
-            $table->string('likable_type', 255);
+            $table->string('likable_type');
             $table->string('user_id', 36)->index();
             $table->timestamps();
             $table->unique(['likable_id', 'likable_type', 'user_id'], 'likeable_likes_unique');
@@ -21,7 +21,7 @@ class CreateLikeableTables extends Migration
         Schema::create('likeable_like_counters', function (Blueprint $table) {
             $table->increments('id');
             $table->string('likable_id', 36);
-            $table->string('likable_type', 255);
+            $table->string('likable_type');
             $table->unsignedInteger('count')->default(0);
             $table->unique(['likable_id', 'likable_type'], 'likeable_counts');
         });
