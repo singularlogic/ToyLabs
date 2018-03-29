499e9cbd6336049fe3c1689dd30cc1693543039f
Christos Botsikas <cbot@epu.ntua.gr>
2018-03-29 03:46:57 +0200

    Remove like widget from analysis view

--- resources/views/product/analysis.blade.php
+++ resources/views/product/analysis.blade.php
@@ -5,12 +5,6 @@
 @section('content')
         <div class="ui main container">
             <h2 class="ui left floated header">{{ $title }}</h2>
-            <likes class="ui right floated"
-                :likes="{{ $meta['likeCount'] }}"
-                :id="{{ $analysis_id }}"
-                liked="{{ $meta['liked'] }}"
-                type="analysis"
-            ></likes>
             <div class="ui clearing divider"></div>
             <analysis
                     :analysis="{{ json_encode($analysis) }}"
