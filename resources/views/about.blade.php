9ccf538f20d49428406a6d8b8766f97eb56020f0
Marios Phinikettos <finikm@gmail.com>
2018-06-13 10:56:09 +0300

    About page is no longer empty (!!!)

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,5 +6,30 @@
         <!-- About -->
         <div class="ui main container">
             <h1 class="ui dividing header">{{ __('About ToyLabs') }}</h1>
+
+            <p>The current platform is being delivered as a product of the <a href="https://www.toylabs.eu/" target="_BLANK">ToyLabs H2020</a> EC c-funded project.</p>
+
+            <p>ToyLabs proposes the development of a unique, unified methodology that will create a multi-stakeholder network and consequently a multi-sided platform where key players in the toy industry value network (i.e. toy manufacturers, FabLabs , Toy Safety experts, childhood professionals, end customers etc.) are brought together and collaborate closely in order to come up with new, innovative toys and games.</p>
+
+            <p>To meet the fundamental challenges of the SME toy manufacturing industry (high costs to address Toy Safety issues, competition with large firms, operation in fragmented markets, high failure rates of new products, late response to market trends and customisation needs), the ΤoyLabs consortium was created, covering in the most representative way the spectrum of stakeholders involved and of the necessary expertise needed, in order to develop an innovative approach aiming at “redefining the game” strengthening the position of SMEs in the toy manufacturing industry.</p>
+
+            <p>The aim is to help the SME toy industry to create toys and games that</p>
+
+            <ul>
+                <li>will be able to quickly enter the market,</li>
+                <li>will respond to a clear market demand,</li>
+                <li>will be cost effective and</li>
+                <li>will be customised  in order to be able to enter also other EU markets.</li>
+            </ul>
+
+            <p>Thereafter, extraordinary impact may arise for toy manufacturing SMEs that, capitalising on their years of experience and expertise as a basis, will decide to make use of ToyLabs innovative methodology and applicable solution that exploits:</p>
+
+            <ul>
+                <li>the FabLabs expertise on rapid prototyping, use of new materials and ICT solutions, providing them with useful recommendations on toy designs and materials to be used as an opportunity to acquire early feedback on the prototypes by participating end customers, reduce time-to-market of new toys and games and enter new markets (exploiting FabLab’s location)</li>
+                <li>the early integration of Toy Safety experts in the process, enabling them to avoid early mistakes in terms of compliance with toy safety directives when the product is ready to enter the market</li>
+                <li>the “crowd wisdom” as reflected by specific involved stakeholder groups (i.e. childhood professionals, parent organisations etc.) that provide them with valuable feedback and social-media audience that provide them with insights on future trends.</li>
+            </ul>
+
+            <p>Therefore, ToyLabs, through the above mentioned concept, that innovates on the value network of SMEs, and based on data analytics, AR technologies, co-creation approaches and feedback management mechanisms, decreases costs of the design and manufacturing process and time to market of new products and supports toy manufacturers reach new markets.</p>
         </div>
 @endsection
