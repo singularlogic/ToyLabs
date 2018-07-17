837c437a6ec6713e2b7cf730466e365a85afda7b
Marios Phinikettos <finikm@gmail.com>
2018-07-17 11:45:00 +0300

    Increased the number of files allowed for AR models

--- resources/assets/js/components/ARModelCreate.vue
+++ resources/assets/js/components/ARModelCreate.vue
@@ -44,6 +44,7 @@
             :useFontAwesome="true"
             :showRemoveLink="true"
             :maxFileSizeInMB="64"
+            :maxNumberOfFiles=50
             paramName="file"
             acceptedFileTypes="image/*,.fbx,.obj,.3ds,.stl,.scn,.dae,.mtl"
             v-on:vdropzone-success="fileAdded"
